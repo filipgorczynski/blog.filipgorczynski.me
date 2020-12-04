e3de03b40b1495782831c3416a12aef3f67c0326
Filip Górczyński <filip.gorczynski@gmail.com>
2020-12-04 07:44:42 +0100

    Play with configuration

---
+++ README.md
@@ -0,0 +1,8 @@
+# Developer blog
+
+## Resources
+
+* https://devhints.io/html-meta
+* https://devhints.io/html
+* https://engineering.zalando.com/posts/2020/07/launching-the-engineering-blog.html
+* 
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,41 +1,59 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+# https://github.com/alexandrevicenzi/Flex/wiki/Configuration-example
 
 AUTHOR = 'Filip Górczyński'
 SITENAME = 'Filip Górczyński'
-SITEURL = 'https://blog.filipgorczynski.me'
-
-PATH = 'content'
-THEME = 'theme'
-# THEME = 'lightweight'
+SITETITLE = "Filip Górczyński"
+SITEURL = 'https://blog.filipgorczynski.me/'
+SITESUBTITLE = "Jack of all trades; Master of one."
+SITEDESCRIPTION = "Jack of all trades; Master of one."
+SITELOGO = SITEURL + "/images/profile.png"
+FAVICON = SITEURL + "/images/favicon.ico"
+BROWSER_COLOR = "#efefef"
+ROBOTS = "index, follow"
+# PATH = 'content'
+THEME = '/home/filip/src/Vendors/pelican-themes/Flex'
 TIMEZONE = 'UTC'
 DEFAULT_LANG = 'pl'
-
+EXTRA_PATH_METADATA = {
+    "extra/custom.css": {"path": "static/custom.css"},
+}
+CUSTOM_CSS = "static/custom.css"
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
+COPYRIGHT_YEAR = 2020
 PAGE_URL = '/{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
-
+USE_FOLDER_AS_CATEGORY = False
 ARTICLE_URL = '/{date:%Y}/{date:%m}/{slug}/'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}/index.html'
+HOME_HIDE_TAGS = True
+MAIN_MENU = True
 
-CSS_FILE = 'main.css'
+# ADD_THIS_ID = "ra-77hh6723hhjd"
+# DISQUS_SITENAME = "yoursite"
+GOOGLE_ANALYTICS = "UA-XXX"
+GOOGLE_TAG_MANAGER = "GTM-XXX"
+
+# CSS_FILE = 'main.css'
 # Blogroll
 LINKS = (
-    ('Stary blog', 'http://filipgorczynski.wordpress.com/'),
+    ('Previous Blog', 'https://filipgorczynski.wordpress.com/'),
 )
 # LINKS = ()
-# Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-#           ('Another social link', '#'),)
 SOCIAL = (
+    ("github", "https://github.com/filipgorczynski"),
+    ("rss", "/blog/feeds/all.atom.xml"),
+)
 
+MENUITEMS = (
+    ("Tech", "/tech.html"),
+    ("Life", "/life.html"),
+    ("Notes", "/notes.html"),
 )
 
 DEFAULT_PAGINATION = 10
@@ -45,12 +63,42 @@ DEFAULT_PAGINATION = 10
 
 # https://www.seyiobaweya.tech/articles/2020-01-20/personal-website-setup/
 # STATIC_PATHS = [
-#     'images', 'content', 'featured', 'logos' 'pdfs', 'static', 'zipfiles'
+#     'images',  'featured', 'logos', # 'content', 'featured', 'logos' 'pdfs', 'static', 'zipfiles'
 # ]
 
 PLUGIN_PATHS = ['/home/filip/src/Vendors/pelican-plugins']
 PLUGINS = [
     'pelican_gist',
+    'pelican-cover-image',
+    'metadataparsing',
+    'pelican_htmlmin',
+    'related_posts',
+    'post_stats',
+    'i18n_subsites',
 ]
 
-# 'pelican_youtube',
+JINJA_ENVIRONMENT = {"extensions": ["jinja2.ext.i18n"]}
+
+# https://github.com/getpelican/pelican-plugins/tree/master/post_stats
+
+# https://github.com/getpelican/pelican-plugins/tree/master/related_posts
+RELATED_POSTS_MAX = 10
+
+OG_LOCALE = "en_EN"
+LOCALE = "en_EN"
+
+# Default theme language.
+I18N_TEMPLATES_LANG = "en"
+# DATE_FORMATS = {
+#     "en": "%B %d, %Y",
+#     "pl": "%Y-%M-%d, %Y",
+# }
+EXTRA_PATH_METADATA = {
+    "extra/ads.txt": {"path": "ads.txt"},
+    "extra/CNAME": {"path": "CNAME"},
+}
+
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+# USE_LESS = True
